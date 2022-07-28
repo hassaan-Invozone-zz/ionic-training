13587ddcce6f4ad758fdc7750924c4b467293920
Hassaan Zahid <hassaan.zahid@invozone.com>
2022-07-28 14:19:02 +0500

    form-submition

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,32 +1,78 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
+import { Component, State, h } from '@stencil/core';
 
 @Component({
   tag: 'my-component',
   styleUrl: 'my-component.css',
-  shadow: true,
 })
 export class MyComponent {
-  /**
-   * The first name
-   */
-  @Prop() first: string;
+  @State() formControls = {
+    firstName: null,
+    lastName: null,
+    age: null,
+    gender: null,
+  };
 
-  /**
-   * The middle name
-   */
-  @Prop() middle: string;
+  changeFormValue(controlName, value) {
+    this.formControls = {
+      ...this.formControls,
+      [controlName]: value
+    };
+  }
 
-  /**
-   * The last name
-   */
-  @Prop() last: string;
-
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
+  handleSubmit(e) {
+    e.preventDefault();
+    fetch("https://webhook.site/941eadb0-f3b7-4330-8e6e-cd7f006e3509", {
+      method: "POST",
+      // Adding body or contents to send
+      body: JSON.stringify(this.formControls),
+    }).then(response => response.json())
+      .then(json => console.log(json));
   }
 
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return (
+      <form onSubmit={(e) => this.handleSubmit(e)}>
+        <label htmlFor="fname">First Name
+          <input required type="text" id="fname" value={this.formControls.firstName} onInput={(e: any) =>
+            this.changeFormValue("firstName",
+              e.target.value)
+          } name="firstname" placeholder="Your first name.."/>
+        </label>
+
+        <label htmlFor="lname">Last Name
+          <input required type="text" id="lname" value={this.formControls.lastName} onInput={(e: any) =>
+            this.changeFormValue("lastName",
+              e.target.value)
+          } name="lastname" placeholder="Your last name.."/>
+        </label>
+
+        <label htmlFor="age">Age
+          <input required type="number" id="age" value={this.formControls.age} onInput={(e: any) =>
+            this.changeFormValue("age",
+              e.target.value)
+          } name="age" placeholder="Age"/>
+        </label>
+
+        <label htmlFor="gender">Gender
+          <select required onInput={(e: any) =>
+            this.changeFormValue("gender",
+              e.target.value)
+          } id="gender" name="gender">
+            <option disabled>--Select--</option>
+            <option selected={this.formControls.gender === 'male'} value="male">Male</option>
+            <option selected={this.formControls.gender === 'female'} value="female">Female</option>
+          </select>
+        </label>
+
+        <label htmlFor="img">Image
+          <input required onInput={(e: any) =>
+            this.changeFormValue("img",
+              e.target.value)
+          } type="file" id="img" name="img" accept="image/*"/>
+        </label>
+
+        <input type="submit" value="Submit"/>
+      </form>
+    );
   }
 }
